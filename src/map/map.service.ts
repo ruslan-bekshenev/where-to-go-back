5e4c364fa03067521d03154a6088ca90917d3094
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-30 23:20:35 +0500

    get place

--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -10,17 +10,19 @@ export class MapService {
   async place(lang: TLang, params: { name: string }) {
     try {
       console.log(
-        `${process.env.API_GEO}/${lang}/places/geoname`,
-        params,
-        process.env.API_GEO_KEY,
+        `${process.env.API_GEO}/${lang}/places/geoname?name=${params.name}&apikey=${process.env.API_GEO_KEY}`,
       );
-      const placesGeoInfo = await axios.get(
-        `${process.env.API_GEO}/${lang}/places/geoname`,
-        { params: { ...params, apikey: process.env.API_GEO_KEY } },
+      console.log(
+        await axios.get(`${process.env.API_GEO}/${lang}/places/geoname`, {
+          params: { name: params.name, apikey: process.env.API_GEO_KEY },
+        }),
       );
-      return placesGeoInfo;
+      const { data } = await axios.get(
+        `${process.env.API_GEO}/${lang}/places/geoname`,
+        { params: { name: params.name, apikey: process.env.API_GEO_KEY } },
+      );
+      return data;
     } catch (e) {
-      console.log(e);
       throw new HttpException('ERR_BAD_REQUEST', HttpStatus.BAD_REQUEST);
     }
   }
