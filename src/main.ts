33f6dbad2e6916f85e6781142f5ade1b3757899e
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-30 22:21:03 +0500

    add config to main ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
+
+const corsOptions: CorsOptions = {
+  allowedHeaders: [
+    'origin',
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'authorization',
+  ],
+  credentials: true,
+  origin: ['http://localhost:3000', 'http://localhost:3000'],
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+  app.enableCors(corsOptions);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+interface IPlaceParams {
+  lang: 'en' | 'ru';
+  name: string;
+}
 
 @Injectable()
-export class MapService {}
+export class MapService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  async place(params: IPlaceParams) {
+    const { lang, ...rest } = params;
+    try {
+      const placesGeoInfo = await axios.get(
+        `${process.env.API_GEO}/${lang}/places/geoname`,
+        { params: rest },
+      );
+      return placesGeoInfo;
+    } catch (e) {
+      throw new HttpException('ERR_BAD_REQUEST', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
