f57cee85be61d7bebcbb41e7a18fd3413daa8336
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-03 03:21:05 +0500

    add endpoints for places

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get, Param, Query, Req } from '@nestjs/common';
-import { TLang } from './map.types';
+import { IRadiusParams, TLang } from './map.types';
 import { MapService } from './map.service';
 
 @Controller('map')
 export class MapController {
   constructor(private mapService: MapService) {}
 
-  @Get('/place/:lang')
-  place(@Param('lang') lang: TLang, @Query() params: { name: string }) {
-    return this.mapService.place(lang, params);
+  @Get('/geoname')
+  place(@Query() params: { name: string }) {
+    return this.mapService.geoname(params);
+  }
+
+  @Get('/radius')
+  radius(@Query() params: IRadiusParams) {
+    return this.mapService.radius(params);
+  }
+
+  @Get('/place-info/:id')
+  placeInfo(@Param('id') id: string) {
+    return this.mapService.placeInfo(id);
   }
 }
--- src/map/map.types.ts
+++ src/map/map.types.ts
@@ -1,1 +1,11 @@
 export type TLang = 'en' | 'ru';
+
+export interface IRadiusParams {
+  radius?: number;
+  limit?: number;
+  offset?: number;
+  lon: number;
+  lat: number;
+  rate?: number;
+  format: 'json' | 'count';
+}
