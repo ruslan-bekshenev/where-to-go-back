83dde31f502d79aa448503a8ba388212f7f6c0af
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-03 03:21:23 +0500

    getting radius, place info

--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -1,28 +1,48 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { TLang } from './map.types';
+import { IRadiusParams, TLang } from './map.types';
 
 @Injectable()
 export class MapService {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 
-  async place(lang: TLang, params: { name: string }) {
+  async geoname(params: { name: string }) {
     try {
-      console.log(
-        `${process.env.API_GEO}/${lang}/places/geoname?name=${params.name}&apikey=${process.env.API_GEO_KEY}`,
-      );
-      console.log(
-        await axios.get(`${process.env.API_GEO}/${lang}/places/geoname`, {
-          params: { name: params.name, apikey: process.env.API_GEO_KEY },
-        }),
-      );
       const { data } = await axios.get(
-        `${process.env.API_GEO}/${lang}/places/geoname`,
+        `${process.env.API_GEO}/ru/places/geoname`,
         { params: { name: params.name, apikey: process.env.API_GEO_KEY } },
       );
       return data;
-    } catch (e) {
+    } catch {
+      throw new HttpException('ERR_BAD_REQUEST', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async radius(params: IRadiusParams) {
+    try {
+      const { data } = await axios.get(
+        `${process.env.API_GEO}/ru/places/radius/`,
+        { params: { ...params, apikey: process.env.API_GEO_KEY } },
+      );
+
+      return data;
+    } catch {
+      throw new HttpException('ERR_BAD_REQUEST', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async placeInfo(id: string) {
+    try {
+      const { data } = await axios.get(
+        `${process.env.API_GEO}/ru/places/xid/${id}`,
+        {
+          params: { apikey: process.env.API_GEO_KEY },
+        },
+      );
+
+      return data;
+    } catch {
       throw new HttpException('ERR_BAD_REQUEST', HttpStatus.BAD_REQUEST);
     }
   }
